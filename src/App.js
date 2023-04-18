dc709440cd146a55395865d2e61511db764c1f26
chisengwong <aaabab239251@gmail.com>
2023-04-18 01:41:31 -0700

    init firebase and connected to creat/edit/delete task features

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 import './App.css';
-
+import {db} from './services/firebase';
+import 'firebase/firestore';
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 function App() {
+  /// Test user ***************************************
+  const temp_userId = "123";
+  const userRef = doc(db, "Users", temp_userId);
+  /********************* TODO: get user id from auth */
+
   const [tasks, setTasks] = useState([]);
 
-  const handleCreate = (task) => {
-    setTasks([...tasks, { id: Date.now(), ...task }]);
+  // Get tasks from db on mount
+  useEffect(() => {
+    async function getTasks() {
+      const userSnap = await getDoc(userRef);
+      if (userSnap.exists()) {
+        setTasks(userSnap.data().tasks);
+      } else { //TODO: usersnap should be created when user signs up
+        await setDoc(userRef, {
+          tasks: []
+        });
+      }
+    }
+    getTasks();
+  }, []);
+
+  async function handleCreate(task){
+    const updatedTasks = [...tasks, { id: Date.now(), ...task }];
+    setTasks(updatedTasks);
+    await updateTask(updatedTasks);
   };
 
-  const handleEdit = (updatedTask) => {
+  async function handleEdit(updatedTask){
     const updatedTasks = tasks.map((task) => {
       if (task.id === updatedTask.id) {
         return updatedTask;
@@ -19,12 +43,20 @@ function App() {
       return task;
     });
     setTasks(updatedTasks);
+    await updateTask(updatedTasks);
   };
 
   const handleDelete = (taskId) => {
   const updatedTasks = tasks.filter((task) => task.id !== taskId);
   setTasks(updatedTasks);
+  updateTask(updatedTasks);
   };
+
+  async function updateTask(updatedTasks){
+    await setDoc(userRef, {
+      tasks: updatedTasks
+    }).catch((error) => { console.error("Error writing document: ", error)})
+  }
 
   return (
   <div>
---
+++ src/services/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyApRGp3kKENzliV8zrIdHWSW-k8j-720kM",
+  authDomain: "task-tracker-f14d6.firebaseapp.com",
+  projectId: "task-tracker-f14d6",
+  storageBucket: "task-tracker-f14d6.appspot.com",
+  messagingSenderId: "833356675452",
+  appId: "1:833356675452:web:6f990f1eeb712fc5c5ccf9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+
+
