53698db633dc5bff8833a6879fff65fec9baa846
Tscheschlog <chris.g.tscheschlog@gmail.com>
2023-04-18 15:57:36 -0400

    login page

---
+++ src/pages/login.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import '../styles/login.css'
+
+const LoginScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // handle login logic here
+  };
+
+  return (
+    <React.Fragment>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+export default LoginScreen;
